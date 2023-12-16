a07a4a7e030ad3ed2c0b685fc7f83726d833b0e0
sanalkumars <sanalskumar126@gmail.com>
2023-12-16 23:55:33 +0530

    first commit for react

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+
+const Header = () => {
+  return (
+    <div>
+        <h2>componenet one</h2>
+    </div>
+  )
+}
+
+export default Header
